8ea4da04c5e4d980b21496115130b8850475ad5a
EinsteinPower <Hongminjung3028@gmail.com>
2023-07-14 14:36:27 +0900

    Footer 컴포넌트 수정

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Main></Main>
-        
+        <Main></Main> 
       </header>
+      <footer className="App-footer"></footer>
     </div>
   );
 }
